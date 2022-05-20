0e2880f7823a4c7636b30275bfda8f695153eac4
Jesús Gutiérrez <jesus.gutierrezmatarrita@outlook.com>
2022-05-19 19:58:51 -0600

    Finish the welcome page

--- template/cabecera.php
+++ template/cabecera.php
@@ -5,7 +5,7 @@
     <meta charset="UFT-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initail-scale=1.0">
-    
+
     <!--bootstrap.min.css-->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <!--animate.css-->

7b9a284cd08ea5ae00e3b5f3536f54e8e1c2c2b6
Jesús Gutiérrez <jesus.gutierrezmatarrita@outlook.com>
2022-05-19 19:25:04 -0600

    Fixed some bugs

--- template/cabecera.php
+++ template/cabecera.php
@@ -1,39 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UFT-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initail-scale=1.0">
+    <meta charset="UFT-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initail-scale=1.0">
 
-<link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <ul class="nav navbar-nav">
-    <li class="nav-item">
-            <a class="nav-link" href="login.html">Inicio </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="listaAtractivos.php">lista atractivos </a>
-        </li>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="">Inicio </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="listaAtractivos.php">Lista atractivos </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="filtroTipoBusqueda.php">Filtro de tipo atractivo/recomendacion</a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="filtroTipoBusqueda.php">Filtro de tipo atractivo/recomendacion</a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contactenos</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Adivinar EA</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Adivinar tipo de profesor</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Clasificación de redes</a>
-        </li>
-    </ul>
-</nav>
-<div class="container">
-    <div class="row">
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contáctenos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Iniciar sesión</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="registrar.php">Crear cuenta</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
+        <div class="row">

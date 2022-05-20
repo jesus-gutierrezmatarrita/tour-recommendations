c6885758977f53eaf3fce384327bef691a5b1f7f
Jesús Gutiérrez <jesus.gutierrezmatarrita@outlook.com>
2022-05-19 19:47:55 -0600

    Finish the welcome page

--- index.php
+++ index.php
@@ -1,38 +1,59 @@
 <?php include("template/cabecera.php"); ?>
 
-<div class="bd-masthead mb-3" id="content">
-    <div class="container-xxl bd-gutter">
-        <section class="col-md-8 mx-auto text-center" src="images/laguna.jpg" data-image-width="256" data-image-height="256" id="welcomeText">
-            <div class="">
-                <h1 class="mb-3 fw-semibold">Esta es Costa Rica</h1>
-                <p>Aquí podrás encontrar las mejores recomendaciones de sitios turísticos</p>
-                <a href="filtroTipoBusqueda.php" class="btn btn-info">Empecemos</a>
+<head>
+    <!--bootstrap.min.css-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--animate.css-->
+    <link rel="stylesheet" href="css/animate.min.css">
+    <!--fontawesome 5-->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!--lity.css-->
+    <link rel="stylesheet" href="css/lity.min.css">
+    <!--slicknav.css-->
+    <link rel="stylesheet" href="css/slicknav.min.css">
+    <!--slickslider.css-->
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- Custom.css -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+</head>
+
+<section class="ws-section-spacing bg-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="center-title">
+                    <h1 class="title">Esta es Costa Rica</h1>
+                    <p class="sub-title" style="margin-bottom: 5%;">Aquí podrás encontrar las mejores recomendaciones de sitios turísticos</p>
+                    <a href="filtroTipoBusqueda.php" class="btn btn-info">Empecemos</a>
+                </div>
             </div>
-        </section>
-    </div>
 
-</div>
-
-<div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
-            <img src="images/pack-1.jpg" class="d-block w-100" alt="landscape1">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-            <img src="images/volcan.jpg" class="d-block w-100" alt="landscape2">
-        </div>
-        <div class="carousel-item">
-            <img src="images/pack-3.jpg" class="d-block w-100" alt="landscape3">
+            <div id="homeCarousel" class="carousel slide col-lg-6" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="2000">
+                        <img src="images/pack-1.jpg" height="200px" alt="landscape1">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="images/volcan.jpg" height="200px" alt="landscape2">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/pack-3.jpg" height="200px" alt="landscape3">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
+</section>
 
 <?php include("template/pie.php"); ?>
--- template/cabecera.php
+++ template/cabecera.php
@@ -5,15 +5,30 @@
     <meta charset="UFT-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initail-scale=1.0">
-
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    
+    <!--bootstrap.min.css-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--animate.css-->
+    <link rel="stylesheet" href="css/animate.min.css">
+    <!--fontawesome 5-->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!--lity.css-->
+    <link rel="stylesheet" href="css/lity.min.css">
+    <!--slicknav.css-->
+    <link rel="stylesheet" href="css/slicknav.min.css">
+    <!--slickslider.css-->
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- Custom.css -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <ul class="nav navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="">Inicio </a>
+                <a class="nav-link" href="index.php">Inicio </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="listaAtractivos.php">Lista atractivos </a>
